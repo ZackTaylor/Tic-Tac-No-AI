d35dd86d51ff5f91bb0ca21448407a33cc3156c1
Zack Tylor <taylor.zackory@gmail.com>
2017-09-06 15:26:19 -0500

    fix bug that allowed user to flip opponents color. Add logic to win the game.

--- src/components/board.js
+++ src/components/board.js
@@ -11,8 +11,32 @@ class Board extends Component {
     };
   }
 
+  calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 6],
+      [1, 4, 7],
+      [2, 5, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ];
+    for (let i = 0; i < lines.length; i++) {
+      const [a, b, c] = lines[i];
+      if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+        return squares[a];
+      }
+    }
+    return null;
+  }
+
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.calculateWinner(squares) || squares[i]) {
+      return;
+    }
+    
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -30,50 +54,40 @@ class Board extends Component {
   }
 
   render() {
-    const status = `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`;
+    const winner = this.calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = "Winner: " + winner;
+    } else {
+      status = `Next player: ${ this.state.xIsNext ? 'X' : 'O' }`;
+    }
       return (
-        <table>
-          <tbody>
-            <tr>
-              { this.renderSquare(0) }
-              { this.renderSquare(1) }
-              { this.renderSquare(2) }
-            </tr>
-            <tr>
-              { this.renderSquare(3) }
-              { this.renderSquare(4) }
-              { this.renderSquare(5) }
-            </tr>
-            <tr>
-              { this.renderSquare(6) }
-              { this.renderSquare(7) }
-              { this.renderSquare(8) }
-            </tr>
+        <div>
 
-          </tbody>
+          <div className="status">{status}</div>
+          <table>
+            <tbody>
+              <tr>
+                { this.renderSquare(0) }
+                { this.renderSquare(1) }
+                { this.renderSquare(2) }
+              </tr>
+              <tr>
+                { this.renderSquare(3) }
+                { this.renderSquare(4) }
+                { this.renderSquare(5) }
+              </tr>
+              <tr>
+                { this.renderSquare(6) }
+                { this.renderSquare(7) }
+                { this.renderSquare(8) }
+              </tr>
+
+            </tbody>
         </table>
+      </div>
     );
   }
-}
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
 }
 
 export default Board;
